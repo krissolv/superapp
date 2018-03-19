b4503c49a903731b4753d665beb1d29c0d1bb794
Kristian Sølvberg <kristian@wtw.no>
2018-03-19 16:45:22 +0100

    Version 2

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-var CACHE_NAME = 'superapp-cache-v1';
+var CACHE_NAME = 'superapp-cache-v2';
 
 var urlsToCache = [
   '/superapp/'
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
+
   event.waitUntil(
       caches.open(CACHE_NAME)
           .then(function(cache) {
