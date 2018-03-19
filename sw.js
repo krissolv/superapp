c300905052ed75b48eaeff6d8dcdd32530364d7f
Kristian Sølvberg <kristian@wtw.no>
2018-03-19 16:21:31 +0100

    Smaller sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,16 @@
+var CACHE_NAME = 'superapp-cache-v1';
+
 var urlsToCache = [
-  '/superapp/',
-  '/superapp/index.html',
-  '/superapp/index.html?launcher=true'
+  '/superapp/'
 ];
 
 self.addEventListener('install', function(event) {
-
+  // Perform install steps
   event.waitUntil(
       caches.open(CACHE_NAME)
           .then(function(cache) {
             console.log('Opened cache');
             return cache.addAll(urlsToCache);
           })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-      caches.match(event.request)
-          .then(function(response) {
-
-                if (response) {
-                  return response;
-                }
-                return fetch(event.request);
-              }
-          )
   );
 });
